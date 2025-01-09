1b6652a5855bc65ec75e7f5733d591f6370eb3e2
Updour <zuliva35@gmail.com>
2025-01-09 14:09:15 +0700

    done task

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,70 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useState, useEffect } from 'react';
+import {  View, Text, FlatList, Image } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+export default HomeScreen = () => {
+  const [data, setData] = useState([])
 
-export default function HomeScreen() {
+  const _handleRetrieveData = async () => {
+    let results = await fetch(`https://dummyjson.com/users?limit=100`)
+    const data = await results.json();
+    const isMap = data.users.filter(items => {
+      let { height, age } = items
+      if (Number(height.toFixed()) < 170 && Math.max(age) ) return items
+      return false
+      })
+      
+    setData(isMap.slice(0, 10));
+  }
+
+  useEffect(() => {
+    _handleRetrieveData()
+  }, [])
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={data}
+        numColumns='2'
+        keyExtractor={item => item.id}
+        renderItem={({item}) => {
+          return (
+            <View style={styles.wrapStyles}>
+              <View style={styles.boxStyles}>
+                <Image
+                  source={{ uri: item.image }}
+                  style={styles.imageStyles}
+                  />
+                  <Text style={[styles.txtStyles, { fontWeight: 'bold' }]}>Name : {item.firstName} {item.lastName} </Text>
+                  <Text style={styles.txtStyles}>Age : {item.age}</Text>
+                  <Text style={styles.txtStyles}>Heigh : {item.height}</Text>
+              </View>
+            </View>
+          )
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
+const styles = {
+  wrapStyles: { 
+    flex: 1,
+    padding: 8,
     flexDirection: 'row',
+    display: 'flex',
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center'
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  boxStyles: {  
+    borderWidth: 1, 
+    borderColor: 'black',
+    borderRadius: 8,
+    marginLeft: 8, 
+    marginRight: 8, 
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  imageStyles: {
+    height: 200, 
+    width: 200
   },
-});
+  txtStyles :{
+    marginLeft: 12
+  }
+}
